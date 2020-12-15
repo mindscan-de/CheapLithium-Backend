a16d93c74e0590229bda019cad6771b955286a7a
mindscan-de <maxim.gansert@gmail.com>
2020-12-15 22:13:56 +0100

    add a method name provider, so that we can provide "code completion"

---
+++ src/de/mindscan/cheaplithium/runtime/DecisionExecutionEngineMethodProvider.py
@@ -0,0 +1,23 @@
+'''
+Created on 15.12.2020
+
+@author: JohnDoe
+'''
+
+class DecisionExecutionEngineMethodProvider(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        pass
+    
+    
+    # TODO. provide a list of transition methods (Decision Node Transition)
+    # TODO: provide a list of node calculation methods (Decision Node)
+    
+    

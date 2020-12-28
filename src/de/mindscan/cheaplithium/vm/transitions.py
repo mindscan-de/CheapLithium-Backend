6e0a80da9a269593d23fc5a59042fba81f87f006
mindscan-de <maxim.gansert@gmail.com>
2020-12-28 18:21:30 +0100

    adding transition functions for exploration of transition method eval

---
+++ src/de/mindscan/cheaplithium/vm/transitions.py
@@ -0,0 +1,17 @@
+'''
+Created on 28.12.2020
+
+@author: JohnDoe
+'''
+
+def isTrue(value):
+    return value==True or str(value).lower()=="true"
+
+def isFalse(value):
+    return value==False or str(value).lower()=="false"
+
+def isYes(value):
+    return value.lowercase()=="yes"
+
+def isNo(value):
+    return value.lowercase()=="no"

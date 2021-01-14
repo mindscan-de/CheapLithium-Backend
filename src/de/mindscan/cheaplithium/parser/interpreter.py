5fb17e0898bdd5fb566f34195bc804a63bde24e8
mindscan-de <maxim.gansert@gmail.com>
2021-01-14 21:45:09 +0100

    start with some strange interpreter, not sure about it yet...

---
+++ src/de/mindscan/cheaplithium/parser/interpreter.py
@@ -0,0 +1,61 @@
+'''
+Created on 14.01.2021
+
+MIT License
+
+Copyright (c) 2021 Maxim Gansert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+@autor: Maxim Gansert, Mindscan
+'''
+SRC_BASE_DIR  = '../../../../../src'
+
+import sys
+sys.path.insert(0,SRC_BASE_DIR)
+
+
+import importlib
+
+from .ast import MethodDeclaration
+
+# interpreterrun = (tree, 'de.mindscan.cheaplithium.vm', 'transitions', {} )
+
+def eval_transition(ast, package, module, environment:dict ):
+    dynamic_module = importlib.import_module('.'+module, package=package)
+    
+    if isinstance(ast, MethodDeclaration):
+        func = getattr(dynamic_module, ast.name)
+        arguments = eval_ll( ast.parameters, environment )
+        
+        # TODO check, whether this is really needed...
+        if arguments is None:
+            arguments=[]
+            
+        return func(*arguments)
+    else:
+        raise Exception("eval_transition can't evaluate {}: (NYI) please implement this type!".format(type(ast)))
+
+# eval lithium language
+def eval_ll( ast, environment):
+    if ast is None:
+        return None
+    
+    raise Exception("eval_ll can't evaluate {}: (NYI) please implement this type!".format())
+    
